daedab6b3de1b4a7b082f80e3e851a463c7b5002
Mhyko Codeniera <138092234+Mikeko24@users.noreply.github.com>
2024-11-20 17:15:58 -0800

    Add files via upload

---
+++ Prelim/Activity III/CODENIERA_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+public class CODENIERA_ACTIVITY_III {
+    public static void main (String [] args){
+        int var1 = 100;
+        String student = "Codeniera";
+        boolean isStudent = true;
+        char characterGrade = 'A';
+        float balance = 400.000f;
+
+        System.out.println(var1);
+        System.out.println(student);
+        System.out.println(isStudent);
+        System.out.println(characterGrade);
+        System.out.println(balance);
+    }
+}
