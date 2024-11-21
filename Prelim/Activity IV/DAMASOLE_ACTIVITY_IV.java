5bd786b28796b3bf57e294765a11358fde63aebb
pewkiepie27 <yeeldamasole@gmail.com>
2024-11-21 12:00:03 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DAMASOLE_ACTIVITY_IV.java
@@ -0,0 +1,11 @@
+public class DAMASOLE_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String one = "My name is";
+        String two = "Auriel Fritz Damasole";
+        String three = "and I am";
+        int age = 18;
+        String five = "years old";
+        System.out.println(one+" "+ two+" "+three+" "+age+" "+five);
+    }
+    /*HELLO ELLIOT */
+}
