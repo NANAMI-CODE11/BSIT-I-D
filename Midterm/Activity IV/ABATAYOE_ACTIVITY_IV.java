bacbcbea47592c6d7c514ed6c86d2aaf3e7203a5
Elboyhub <elboyabatayo04@gmail.com>
2024-11-21 10:25:04 +0800

    Add files via upload

---
+++ Midterm/Activity IV/ABATAYOE_ACTIVITY_IV.java
@@ -0,0 +1,13 @@
+public class ABATAYOE_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+        String E = "My name is ";
+        String L = "e delete nyani bungkag kaugmaon nimo ";
+        String B = "suwayi lang ";
+        int O = 22;
+        String Y = "years old";
+
+        System.out.println(E+L+B+O+Y);
+
+    }
+}
