5762c78aca37c0577db6f3721b469dff72678e88
pewkiepie27 <yeeldamasole@gmail.com>
2024-11-21 11:59:24 +0800

    Add files via upload

---
+++ Prelim/Activity III/DAMASOLE_ACTIVITY_3.java
@@ -0,0 +1,20 @@
+public class DAMASOLE_ACTIVITY_3 {
+    
+    public static void main(String[]args) {
+        
+        int var1 = 1;
+        String World = "Earth";
+        boolean TurnOn = true;
+        char plus_sign = '+';
+        float money = 200.70f;
+        System.out.println(var1); 
+        System.out.println(World);
+        System.out.println(TurnOn);
+        System.out.println(plus_sign);
+        System.out.println(money);
+        
+
+
+    }
+    
+}
