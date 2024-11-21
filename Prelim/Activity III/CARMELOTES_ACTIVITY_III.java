8be1cc50558ef6564a99a74724f36237aa604977
Arvi002 <mark.arvi15@gmail.com>
2024-11-21 12:08:33 +0800

    Add files via upload

---
+++ Prelim/Activity III/CARMELOTES_ACTIVITY_III.java
@@ -0,0 +1,26 @@
+class CARMELOTES_PROG1_ACTIVITY_III {
+    public static void main (String[] args) {
+    String name = "Arvi";
+    int age = 18;
+    double height = 1.58;
+    float weight =58f;
+    char letter = 'j';
+ 
+    
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(weight);
+    System.out.println(letter);
+    
+ 
+ 
+ 
+ 
+ 
+    }
+ 
+ }
+ 
+    
+
