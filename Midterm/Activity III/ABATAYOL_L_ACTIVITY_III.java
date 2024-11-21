61fcf1b724cbfa533777fe43b37ccdbb64541000
Leonard Abatayo <leonardabatayo5@gmail.com>
2024-11-21 10:36:04 +0800

    Add files via upload

---
+++ Midterm/Activity III/ABATAYOL_L_ACTIVITY_III.java
@@ -0,0 +1,9 @@
+public class ABATAYOL_L_ACTIVITY_III {
+    public static void main (String [] args){
+       int x = 5;
+        while (x >= 0){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
