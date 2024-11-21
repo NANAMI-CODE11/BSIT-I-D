51f269bae8c8aeb965e10c18517d791e2e244665
urninin05 <alfeeninarestauro@gmail.com>
2024-11-21 09:16:02 +0800

    Add files via upload

---
+++ Midterm/RESTAURO_PROG_1.java
@@ -0,0 +1,18 @@
+class RESTAURO_ACTIVITY_III {
+    public static void main(String [] args) {
+        String name = "Alfee";
+        int age = 19;
+        float  height = 155.5f;
+        double weight = 45;
+        char letter = 'n';
+    
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        
+    
+    }
+}
