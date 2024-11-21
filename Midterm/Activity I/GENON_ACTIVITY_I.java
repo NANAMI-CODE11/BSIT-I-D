bd410162ad02059194b80d3eec004b32d355fd08
JOAMIE B. GENON <joamiegenon50@gmail.com>
2024-11-21 10:16:39 +0800

    Add files via upload

---
+++ Midterm/Activity I/GENON_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class GENON_ACTIVITY_I {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a color among red, green, or black: ");
+    String color = sc.nextLine();
+        switch (color) {
+            case "red":
+            System.out.println("Box #1");    
+                break;
+            case "green":
+            System.out.println("Box #2");
+                break;
+            case "black":
+            System.out.println("Box #3");
+                break;
+        }
+     sc.close();
+    }
+
+}
