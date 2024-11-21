3f57f001d1024b580dbadd28315dbf13ca34bc27
KyZn777 <nielznkyth007@gmail.com>
2024-11-21 11:45:40 +0800

    Add files via upload

---
+++ Midterm/Activity III/public class MontesClarosActivityIII {.java
@@ -0,0 +1,9 @@
+public class MontesClarosActivityIII {
+    public static void main(String[] args) {
+        int counter = 5;
+        while (counter >= 0) {
+            System.out.println(counter);
+            counter--;
+        }
+    }
+}
