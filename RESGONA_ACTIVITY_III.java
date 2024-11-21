ad5ae84ce5a5e756bee1ab08e0f154bf6cffff2c
Beangkot <beangresgona@gmail.com>
2024-11-21 09:20:56 +0800

    Add files via upload

---
+++ RESGONA_ACTIVITY_III.java
@@ -0,0 +1,23 @@
+class RESGOÑA_ACTIVITY_III {
+    public static void main ( String [] args) {
+    String name = "Bea";
+    int age = 11;
+    double weight = 14.3;
+    float height = 149.53f;
+    char letter = 'B';
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(weight);
+    System.out.println(height);
+    System.out.println(letter);
+
+    } 
+}
+   
+
+
+
+
+
+
