ed6dbf8ae69a6de8f381a5961d2e7508ebbf3d51
rhenzvyopada <Rhenzvyopada1@gmail.com>
2024-11-21 09:23:57 +0800

    Add files via upload

---
+++ Prelim/Activity IV/OPADA_ACTIVITY_IV.java
@@ -0,0 +1,13 @@
+class OPADA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String A = "my name is ";
+        String B = "Rhenzvy ";
+        String C = "and I am ";
+        int age = 18;
+        String suffex = " years old";
+
+        System.out.println(A+B+C+age+suffex);
+
+    }
+    
+}
