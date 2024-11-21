831edc305774985daa21c0306a87aa48b4d91585
Beangkot <beangresgona@gmail.com>
2024-11-21 09:39:07 +0800

    Add files via upload

---
+++ Midterm/Activity I/RESGONA_ACTIVITY_I.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class cc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter a color (red,green,black)");
+        String color = scanner.nextLine();
+        switch (color){
+            case "red":
+            System.out.println(1);
+            break;
+            case "green":
+            System.out.println (2);
+            break;
+            case "black":
+            System.out.println (3);
+            break;
+            default:
+            System.out.println ("Invalid color");
+      
+        } 
+       
+        }
+    }
