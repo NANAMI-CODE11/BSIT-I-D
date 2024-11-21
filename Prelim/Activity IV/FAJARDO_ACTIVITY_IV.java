9a204a5628ae8a40dc3ebd1d6a0c58045e682b92
Emerson Fajardo <bambipansoy@gmail.com>
2024-11-21 11:59:18 +0800

    Add files via upload

---
+++ Prelim/Activity IV/FAJARDO_ACTIVITY_IV.java
@@ -0,0 +1,11 @@
+class FAJARDO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String q = " My name is";
+        String h = " Emerson";
+        String e = " and I am ";
+        int age = 22;
+        String r = " years old";
+        String t = q+h+e+age+r;
+        System.out.println(t);
+    }
+}
