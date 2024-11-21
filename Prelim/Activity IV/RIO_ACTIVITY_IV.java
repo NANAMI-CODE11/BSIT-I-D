fab18d4a46b4bf5c0e26635a4b9009c9eb3f6cad
Renn933 <rennanboy345@gmail.com>
2024-11-21 12:00:17 +0800

    Add files via upload

---
+++ Prelim/Activity IV/RIO_ACTIVITY_IV.java
@@ -0,0 +1,14 @@
+class RIO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String A = "my name is ";
+        String B = "Rennan ";
+        String C = "and I am ";
+        int age = 18;
+        String suffex = " years old";
+
+        System.out.println(A+B+C+age+suffex);
+
+    }
+    
+}
+
