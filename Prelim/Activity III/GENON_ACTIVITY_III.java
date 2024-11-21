146d3663f3eb6f0e38c295b57c83b5f147260e81
JOAMIE B. GENON <joamiegenon50@gmail.com>
2024-11-21 09:38:01 +0800

    Add files via upload

---
+++ Prelim/Activity III/GENON_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class GENON_ACTIVITY_III {
+    public static void main(String[] args) {
+        
+        String name;
+        name = "Joamie"
+        System.out.println(name);
+        int age = 20;
+        System.out.println (age);
+        double weight = 45.6;
+        System.out.println (weight);
+        float height = 167.52f;
+        System.out.println (height);
+        char letter = 'B';
+        System.out.println (letter)
+
+    }
+}
