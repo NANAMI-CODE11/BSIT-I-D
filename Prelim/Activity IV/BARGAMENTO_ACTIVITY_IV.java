d8dc07836fe5a1a61a23bc7f69f280f7490843e6
crisbarga11 <cristymaebargamento@gmail.com>
2024-11-21 11:59:59 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BARGAMENTO_ACTIVITY_IV.java
@@ -0,0 +1,16 @@
+public class BARGAMENTO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        
+        String a ="My name is ";
+        String b ="Cristy Mae ";
+        String c = "and I am ";
+        int age = 19;
+        String d =" years old.";
+
+        System.out.println(a+b+c+age+d);
+
+
+
+    }
+
+}
