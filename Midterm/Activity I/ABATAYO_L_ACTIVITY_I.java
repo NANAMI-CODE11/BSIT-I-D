c0b47458d48fcfb9f4284eacdde95d6d237792e2
Leonard Abatayo <leonardabatayo5@gmail.com>
2024-11-21 11:21:51 +0800

    Update ABATAYO_L_ACTIVITY_I.java

--- Midterm/Activity I/ABATAYO_L_ACTIVITY_I.java
+++ Midterm/Activity I/ABATAYO_L_ACTIVITY_I.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ABATAYOL_L_ACTIVITY_I {
+public class ABATAYO_L_ACTIVITY_I {
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("------------------");
