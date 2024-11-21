2bd85687f62a871b801983382716164dc34522e3
Erich Panoncillon <zoeypanoncillon@gmail.com>
2024-11-21 10:56:19 +0800

    Add files via upload

---
+++ Midterm/Activity III/PANONCILLON_ACTIVITY_III {.java
@@ -0,0 +1,9 @@
+public class PANONCILLON_ACTIVITY_III {
+    public static void main (String [] args){
+       int x = 5;
+        while (x >= 0){
+            System.out.println(x);
+            x--;
+        }
+    }
+  }
