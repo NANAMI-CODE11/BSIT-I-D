7041aab9d7bb1c261f0d3d5760c0934822400c93
JOAMIE B. GENON <joamiegenon50@gmail.com>
2024-11-21 09:48:51 +0800

    Add files via upload

---
+++ Prelim/GENON_ACTIVITY_IV.java
@@ -0,0 +1,14 @@
+public class GENON_ACTIVITY_IV {
+    public static void main(String[] args) {
+        
+        String a = "My name is";
+        String b = "Joamie";
+        String c = "and I am";
+        int age = 20;
+        String d = "years old";
+
+        System.out.println(a+b+c+age+d);
+    }
+
+
+    }
