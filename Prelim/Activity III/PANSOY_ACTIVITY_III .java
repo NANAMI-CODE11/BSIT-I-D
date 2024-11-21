f9793f00f2cca2d9020af59b85be6de2d3fd958b
pansoyjchub <jamesclydepansoy23@gmail.com>
2024-11-21 12:03:16 +0800

    Delete Prelim/Activity III/PANSOY_ACTIVITY_III .java

--- Prelim/Activity III/PANSOY_ACTIVITY_III .java
+++
@@ -1,9 +0,0 @@
-public class PANSOY_ACTIVITY_III {
-    public static void main(String [] args){
-        int seconds = 5;
-        while(seconds>=0){
-            System.out.println(seconds);
-            seconds--;
-        }
-    }
-}
