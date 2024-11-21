0c98dc7fb7880830c6219e53af63dc953073943e
rhenzvyopada <Rhenzvyopada1@gmail.com>
2024-11-21 09:22:02 +0800

    Add files via upload

---
+++ Prelim/Activity III/OPADA_ACTIVITY_III.java
@@ -0,0 +1,19 @@
+class OPADA_ACTIVITY_III {
+    public static void main (String[] args) {
+String name = "renzu";
+int age = 18;
+float number = 164.2f;
+char code = 'R';
+boolean player = true;
+double height = 5.5;
+
+System.out.println(name);
+System.out.println(age);
+System.out.println(number);
+System.out.println(code);
+System.out.println(player);
+System.out.println(height);
+
+    }
+}
+
