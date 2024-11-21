c458cd6a8c1d3f46d264ad0a4fd1a3c7eaade8a0
Carl M. Moscoso <lyrichkrl@gmail.com>
2024-11-21 12:00:13 +0800

    Delete Prelim/Activity III/MOSCOSO_ACTIVITY_III.java

--- Prelim/Activity III/MOSCOSO_ACTIVITY_III.java
+++
@@ -1,9 +0,0 @@
-public class MOSCOSO_ACTIVITY_III {
-  public static void main (String [] args){
-     int x = 5;
-      while (x >= 0){
-          System.out.println(x);
-          x--;
-      }
-  }
-}
