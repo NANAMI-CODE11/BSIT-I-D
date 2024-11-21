019e743d62d7c4cd1746d6c27558c57be66f3439
loujey <185305137+loujey@users.noreply.github.com>
2024-11-21 11:59:22 +0800

    Add files via upload

---
+++ Prelim/Activity III/MAHINAY_ACTIVITY _III.java
@@ -0,0 +1,21 @@
+class MAHINAY_ACTIVITY_III {
+    public static void main (String[] args) {
+    String name = "Loujemay";
+    int age = 18;
+    double height = 1.55;
+    float weight = 45f;
+    char letter = 'l';
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(height);
+    System.out.println(weight);
+    System.out.println(letter);
+
+   
+
+
+
+    }
+
+}
