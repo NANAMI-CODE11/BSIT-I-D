ced16ac1982e73b09617bf3d8c881f5fed237ccb
Jaybee Daroy <daroyjaybee47@gmail.com>
2024-11-21 11:18:40 +0800

    Add files via upload

---
+++ Prelim/Activity IV/DAROY_ACTIVITY_IV.java
@@ -0,0 +1,13 @@
+public class DAROY_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String a = "My name is ";
+        String name = "Jaybee ";
+        String b = "and I am ";
+        int age = 18;
+        String c = " years old. ";
+        String d = a+name+b+age+c;
+
+        System.out.println(d);
+    }
+}   
+
