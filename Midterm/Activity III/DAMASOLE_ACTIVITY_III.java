375b8b5563711c83d3d8dfb525ab7edb26c95293
pewkiepie27 <yeeldamasole@gmail.com>
2024-11-21 09:28:55 +0800

    Add files via upload

---
+++ Midterm/Activity III/DAMASOLE_ACTIVITY_III.java
@@ -0,0 +1,9 @@
+public class DAMASOLE_ACTIVITY_III {
+    public static void main(String[] args) {
+        int countdown = 5;
+        while (countdown >= 0) {
+            System.out.println(countdown);
+            countdown --;
+        }
+    }
+}
