730737fb86ede9ff2dca0c2571ac63b727990df3
VILLARINFRANZEN123 <margajafranzen@gmail.com>
2024-11-21 09:26:19 +0800

    Add files via upload

---
+++ Midterm/Activity I/VILLARIN_ACTIVITY_I.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class VILLAREN_ACTIVITY_I{ 
+public static void main(Strings[] args){
+    Scanner sc = new Scanner(System.in);
+    String Color = sc.nextline();
+
+    System.out.println("Enter the next color of the item: ");
+
+    switch(color){
+    case "red":
+    System.out.println(1);
+    break;
+    case "green":
+    System.out.println(2);
+    break;
+    case "black":
+    System.out.println(3);
+    break;
+
+
+}
+
+}
+
+
+
+
+
