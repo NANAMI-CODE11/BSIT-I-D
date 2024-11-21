bd320f0eab8d309685653ed874706501f9ec34d6
pansoyjchub <jamesclydepansoy23@gmail.com>
2024-11-21 12:03:44 +0800

    Add files via upload

---
+++ Prelim/Activity III/PANSOY_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+class PANSOY_ACTIVITY_III {
+    public static void main (String[] args) {
+String coke = "royal";
+int tagpila = 15;
+double ako = 15.6;
+boolean eyy = (true);
+char siya = 'P';
+System.out.println(coke);
+System.out.println(tagpila);
+System.out.println(ako);
+System.out.println(eyy);
+System.out.println(siya);
+    }
+}
