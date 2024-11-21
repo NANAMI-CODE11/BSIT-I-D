7988aa6237c16ebfd854abac05687c6e87d5c848
JOAMIE B. GENON <joamiegenon50@gmail.com>
2024-11-21 10:18:15 +0800

    Add files via upload

---
+++ Midterm/Activity III/GENON_ACTIVITY_III.java
@@ -0,0 +1,9 @@
+public class GENON_ACTIVITY_III {
+    public static void main(String [] args){
+        int seconds = 5;
+        while(seconds>=0){
+            System.out.println(seconds);
+            seconds--;
+        }
+    }
+}
