5346f606b9f18a298d23578fa8b22e0682afdcea
Mhyko Codeniera <138092234+Mikeko24@users.noreply.github.com>
2024-11-20 17:16:37 -0800

    Add files via upload

---
+++ Prelim/Activity IV/CODENIERA_ACTIVITY_IV.java
@@ -0,0 +1,13 @@
+public class CODENIERA_ACTIVITY_IV {
+    public static void main (String [] args){
+        String one = "My name is";
+        String two = " Mhyko Codeniera";
+        String three = " and I am ";
+        int age = 18;
+        String four = " years old";
+
+        System.out.println(one + two + three + age + four);
+
+
+    }
+}
