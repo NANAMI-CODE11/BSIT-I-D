dfdb99893aec15eb7afa8a9840293637dfbdb2f3
Yrman Jhon D. Tabucanon <Yrmanjhon13@gmail.com>
2024-11-21 09:14:28 +0800

    Add files via upload

---
+++ Prelim/Activity III/TABUCANON_ACITIVITY_III.java
@@ -0,0 +1,18 @@
+class TABUCANON_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Urasashi";
+        int age = 18;
+        float number = 5.7f;
+        char code = 'X';
+        boolean arise = true;
+        double height = 5.8;
+
+        System.out.println(name);
+        System.out.println(height);
+        System.out.println(code);
+        System.out.println(number);
+        System.out.println(arise);
+        System.out.println(age);
+
+    }
+}
