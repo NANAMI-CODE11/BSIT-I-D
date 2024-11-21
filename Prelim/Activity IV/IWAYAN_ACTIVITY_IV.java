9f80589e5dad1366df9d30762b7db7ed5fc51fb4
Irnan Meynard Iwayan <iirnanmeynard@gmail.com>
2024-11-20 18:31:50 -0800

    Add files via upload

---
+++ Prelim/Activity IV/IWAYAN_ACTIVITY_IV.java
@@ -0,0 +1,13 @@
+public class IWAYAN_ACTIVITY_IV {
+        public static void main(String[] args) {
+            String a = "My name is ";
+            String b = "Irnan ";
+            String c = "and I am ";
+            int age = 20;
+            String d = " years old";
+            
+            System.out.println(a+b+c+age+d);
+
+
+        }
+}
