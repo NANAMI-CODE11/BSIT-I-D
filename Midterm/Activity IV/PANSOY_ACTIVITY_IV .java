24cd3176cc09cc167a1f8ac20f530e0c0999ed1c
pansoyjchub <jamesclydepansoy23@gmail.com>
2024-11-21 10:57:40 +0800

    Add files via upload

---
+++ Midterm/Activity IV/PANSOY_ACTIVITY_IV .java
@@ -0,0 +1,21 @@
+public import java.util.Scanner;
+
+class PANSOY_ACTIVITY_IV {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a non-negative integer:");
+        int num = sc.nextInt();
+
+        int factorial = 1;
+
+        for (int i=1; i<=num; i++) {
+            factorial*=i;
+    } 
+    System.out.println(factorial); 
+  }
+} 
+    
+
+
+
