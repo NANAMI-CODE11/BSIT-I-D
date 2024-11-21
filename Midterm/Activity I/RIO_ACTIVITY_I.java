2266d19be7cdb5cf927e4a7a410293273aad27af
Renn933 <rennanboy345@gmail.com>
2024-11-21 10:04:18 +0800

    Update RIO_ACTIVITY_I.java

--- Midterm/Activity I/RIO_ACTIVITY_I.java
+++ Midterm/Activity I/RIO_ACTIVITY_I.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class RIO_ACTIVITY_1 {
+class RIO_ACTIVITY_I {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose a color between Red,Green,Black:");
