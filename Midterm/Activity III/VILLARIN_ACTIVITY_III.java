358d733d30c34b5c3895337006d2b195e37e0c05
VILLARINFRANZEN123 <margajafranzen@gmail.com>
2024-11-21 09:29:12 +0800

    Add files via upload

---
+++ Midterm/Activity III/VILLARIN_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class VILLARIN_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        while (num >= 0) {
+              System.out.println(num);
+              num=num-1;
+
+        }
+    }
+  }
+
