b5ac89125d11b733c97c81cca2d1d7287b41ba6f
urninin05 <alfeeninarestauro@gmail.com>
2024-11-21 09:50:31 +0800

    Add files via upload

---
+++ Prelim/Activity III/RESTAURO_ACTIVITY_III.java
@@ -0,0 +1,12 @@
+class RESTAURO_ACTIVITY_III{
+    public static void main(String[] args) {
+        String name = "Alfee";
+        int age= 18 ;
+        double weight = 45;
+        float letter= 'A';
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(weight);
+    System.out.println(letter);
+    }
+}   
