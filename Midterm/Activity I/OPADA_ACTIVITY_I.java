edb7c6472121badb8b7a1af3bc1cca37201e6476
rhenzvyopada <Rhenzvyopada1@gmail.com>
2024-11-21 09:25:42 +0800

    Add files via upload

---
+++ Midterm/Activity I/OPADA_ACTIVITY_I.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class OPADA_ACTIVITY_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("(Red, Green, and Black)");
+        System.out.println("Enter the color choices:");
+        String color = sc.nextLine();
+        switch(color) {
+            case "Red":
+            System.out.println("This goes to box 1");
+            break;
+            case "Green":
+            System.out.println("This goes to box 2");
+            break;
+            case "Black":
+            System.out.println("This goes to box 3");
+            break;
+            default:
+            System.out.println("Enter the choices that are shown");
+
+        }
+        
+
+
+
+    }
+}
