2d661043da28e56a408b43dd2d2ae1b223e01298
Leonard Abatayo <leonardabatayo5@gmail.com>
2024-11-21 10:35:16 +0800

    Add files via upload

---
+++ Midterm/Activity I/ABATAYOL_L_ACTIVITY_I.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ABATAYOL_L_ACTIVITY_I {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("------------------");
+        System.out.println("Red Green Black");
+        System.out.println("------------------");
+        System.out.println("Enter a Color:");
+        String color = sc.nextLine();
+            switch (color) {
+                case "Red":
+                    System.out.println("Box #1");
+                    break;
+                case "Green":
+                    System.out.println("Box #2");
+                    break;
+                case "Black":
+                    System.out.println("Box #3");
+                    break;
+            
+                default:
+                    System.out.println("Enter a valid color");
+                    break;
+            }
+        sc.close();
+    }
+}
