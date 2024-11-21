065e70d5404235f0176b82e8605bad291253753c
Elboyhub <elboyabatayo04@gmail.com>
2024-11-21 10:32:52 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ABATAYO_EACTIVITY_IV.java
@@ -0,0 +1,13 @@
+public class ABATAYO_EACTIVITY_IV {
+    public static void main (String [] args){
+        String one = "My name is";
+        String two = " Elboyins";
+        String three = " and I am ";
+        int age = 22;
+        String four = " years old";
+
+        System.out.println(one + two + three + age + four);
+
+
+    }
+}
