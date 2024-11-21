5a0a7a9065df82bd06c8d1f084df8af764bb75b1
loujey <185305137+loujey@users.noreply.github.com>
2024-11-21 10:29:19 +0800

    Add files via upload

---
+++ Prelim/Activity IV/MAHINAY_ACTIVITY_IV.java
@@ -0,0 +1,18 @@
+public class MAHINAY_ACTIVITY_IV {
+public static void main (String[] args) {
+    String a = "My name is ";
+    String b = "jen ";
+    String c = "and I am ";
+    int age = 18;
+    String d = " years old.";
+
+   
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+
+    System.out.println(a+b+c+age+d);
+
+    
+}
+}
