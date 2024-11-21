196974b12d2d1e7f007350c12e7751a96fee65f5
Renn933 <rennanboy345@gmail.com>
2024-11-21 09:40:30 +0800

    Add files via upload

---
+++ Midterm/Activity III/RIO_ACTIVITY_III.java
@@ -0,0 +1,9 @@
+public class RIO_ACTIVITY_III {
+    public static void main(String [] args){
+        int seconds = 5;
+        while(seconds>=0){
+            System.out.println(seconds);
+            seconds--;
+        }
+    }
+}
