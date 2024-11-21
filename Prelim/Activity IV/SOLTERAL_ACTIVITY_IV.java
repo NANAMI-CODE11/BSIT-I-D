428d09baaabaead66905235ae4edf4a50d6a516d
JayfredSolteral <jayfredsolteral074@gmail.com>
2024-11-21 09:31:42 +0800

    Add files via upload

---
+++ Prelim/Activity IV/SOLTERAL_ACTIVITY_IV.java
@@ -0,0 +1,12 @@
+class SOLTERAL_ACTIVITY_IV {
+    public static void main(String[] args) {
+    String a ="My name is ";
+    String b ="Jayfred";
+    String c =" and i am ";
+    int age = 18 ;
+    String suffex =" years old";
+
+    System.out.println(a+b+c+age+suffex);
+      
+    }
+} 
