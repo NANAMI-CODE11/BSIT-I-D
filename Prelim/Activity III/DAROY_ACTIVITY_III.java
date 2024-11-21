0a59fb6eb6681d92a0ec1bbab74670290416da32
Jaybee Daroy <daroyjaybee47@gmail.com>
2024-11-21 12:03:06 +0800

    Add files via upload

---
+++ Prelim/Activity III/DAROY_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class DAROY_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Jaybee";
+        char letter = 'J';
+        float height = 1.60f;
+        int age = 22;
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(letter);
+        System.out.println(height);
+        System.out.println(age);
+        System.out.println(isOpen);
+    }
+}
