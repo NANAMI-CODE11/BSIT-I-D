6b974d98c005a9d00b0052a771d75a9ba1e0171b
pansoyjchub <jamesclydepansoy23@gmail.com>
2024-11-21 10:31:40 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PANSOY_ACTIVITY_IV.java
@@ -0,0 +1,14 @@
+public class PANSOY_ACTIVITY_IV {
+        public static void main(String[] args) {
+            String a = "My name is ";
+            String name = "Julienne ";
+            String b = "and I am ";
+            int age = 22;
+            String c = " years old. ";
+            String d = a+name+b+age+c;
+    
+            System.out.println(d);
+        }
+    }
+
+    
