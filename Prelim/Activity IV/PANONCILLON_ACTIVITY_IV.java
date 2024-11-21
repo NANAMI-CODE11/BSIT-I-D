ae8fd899664e1ec9fd6b183a4339a30208307912
Erich Panoncillon <zoeypanoncillon@gmail.com>
2024-11-21 10:52:01 +0800

    Add files via upload

---
+++ Prelim/Activity IV/PANONCILLON_ACTIVITY_IV.java
@@ -0,0 +1,14 @@
+public class PANONCILLON_ACTIVITY_IV {
+    public static void main (String [] args){
+        String one = "My name is";
+        String two = "Erich";
+        String three = " and I am ";
+        int age = 19;
+        String four = " years old";
+
+        System.out.println(one + two + three + age + four);
+
+
+    }
+}
+
