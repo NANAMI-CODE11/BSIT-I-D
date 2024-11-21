6472bb5de57b16e2d57418d1dcb0c61fa57607a7
Arvi002 <mark.arvi15@gmail.com>
2024-11-21 10:04:02 +0800

    Update CARMELOTES_ACTIVITY_I.java

--- Midterm/Activity I/CARMELOTES_ACTIVITY_I.java
+++ Midterm/Activity I/CARMELOTES_ACTIVITY_I.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CODENIERA_ACTIVITY_I {
+public class CARMELOTES_ACTIVITY_I {
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("------------------");
