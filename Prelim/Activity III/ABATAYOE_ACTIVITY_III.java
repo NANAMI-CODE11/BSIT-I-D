a5bfa2c43727af3ff9ac12040bc535889d164a42
Elboyhub <elboyabatayo04@gmail.com>
2024-11-21 09:42:54 +0800

    Add files via upload

---
+++ Prelim/Activity III/ABATAYOE_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+public class ABATAYO_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Elboyins";
+        int age = 22;
+        double height = 5.0;
+        boolean gege = (false);
+        char lang= 'A';
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(gege);
+        System.out.println(lang);
+    }
+}
