eaa56a8d0fdca02ce3bdd7581bb1f154a6d9e3f8
VILLARINFRANZEN123 <margajafranzen@gmail.com>
2024-11-21 09:20:54 +0800

    Add files via upload

---
+++ Prelim/Activity IV/VILLARIN_ACTIVITY_IV.java
@@ -0,0 +1,14 @@
+public class VILLARIN_ACTIVITY_IV {
+    public static void main(String[] args){
+        String name = "my name is" ;
+        String secondname = "Franzen";
+        String thirdname = "and i am";
+        String age = "18";
+        String lastly = "years old";
+    System.out.println(name+" "+secondname+" "+thirdname+" "+age+" "+lastly);
+    }
+    
+
+        
+    }
+
