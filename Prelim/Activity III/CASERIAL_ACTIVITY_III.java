e3c59236bc92b4efb41d4d847f0b9f5bb111a3a7
mariniel1 <caserialmariniel33@gmail.com>
2024-11-22 23:58:47 -0800

    Add files via upload

---
+++ Prelim/Activity III/CASERIAL_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+class CASERIAL_ACTIVITY_III {
+    public static void main(String [] args) {
+        String name = "Mariniel";
+        int age = 18;
+        float  height = 144.78f;
+        double weight = 30;
+        char letter = 'm';
+    
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+
+    }
+}    
