128045ce67a5cc96d0b05159176bacf654dd0dce
Krsitian Calvin Villacampo <calvinyawa18@gmail.com>
2024-11-28 09:40:48 +0800

    Add files via upload

---
+++ Prelim/Activity IV/VILLACAMPO_ACTIVITY_IV..java
@@ -0,0 +1,10 @@
+class VILLACAMPO_ACTIVITY_IV {
+    public static void main (String[] args) {
+String what = "My name is";
+String who = "Kristian";
+String who2 = "and I am";
+int age = 18;
+String ha = "years old.";
+System.out.println(what+" "+who+" "+who2+" "+age+" "+ha);
+    }
+    }
