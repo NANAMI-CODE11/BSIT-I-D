4bfd9e10fe455dd6cc427c59da19f6db67952ab8
VILLARINFRANZEN123 <margajafranzen@gmail.com>
2024-11-28 09:53:26 +0800

    Add files via upload

---
+++ Midterm/Activity III/VILLARIN_ACTIVITY_III.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VILLARIN_ACTIVITY_III {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of seconds to countdown: ");
+        int seconds = sc.nextInt();
+
+        while (seconds >= 0) {
+            System.out.println(seconds);
+            seconds--;
+        }
+
+        sc.close();
+    }
+}
