e76aa658cd2f14ff09cd1e789ace739ca6618626
precyromera <dragonsandprincess@gmail.com>
2024-11-28 09:38:23 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ROMERA_ACTIVITY_IV.java
@@ -0,0 +1,12 @@
+public class ROMERA_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String a = "My name is ";
+        String name = "Precy ";
+        String b = "and I am ";
+        int age = 18;
+        String c = " years old. ";
+        String d = a+name+b+age+c;
+
+        System.out.println(d);
+    }
+}
