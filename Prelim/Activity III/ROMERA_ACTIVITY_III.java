e0f1511545aae49bda4f2b78bea1ac860872df7d
precyromera <dragonsandprincess@gmail.com>
2024-11-28 09:37:01 +0800

    Add files via upload

---
+++ Prelim/Activity III/ROMERA_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class ROMERA_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Precy";
+        char letter = 'P';
+        float height = 1.75f;
+        int age = 18;
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(letter);
+        System.out.println(height);
+        System.out.println(age);
+        System.out.println(isOpen);
+    }
+}
