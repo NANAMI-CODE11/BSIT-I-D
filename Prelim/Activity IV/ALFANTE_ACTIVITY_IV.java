b4628df73e24bb6321f4b54fc15c0be318ca0141
Zierah Alfante <alfantezierah@gmail.com>
2024-11-28 09:49:03 +0800

    Add files via upload

---
+++ Prelim/Activity IV/ALFANTE_ACTIVITY_IV.java
@@ -0,0 +1,13 @@
+public class ALFANTE_ACTIVITY_IV {
+    public static void main(String [] args){
+
+        String name1 = "My name is";
+        String name2 =  "Zierah";
+        String name3 =  "and I am";
+        int age = 19;
+        String name4 = "years old.";
+
+        System.out.println(name1+" "+name2+" "+name3+" "+age+" "+name4);
+    }
+    
+}
