7091a6a15516474358f3eb1e0b34dc01ee3aa629
Leonard Abatayo <leonardabatayo5@gmail.com>
2024-11-28 09:06:31 +0800

    Add files via upload

---
+++ Prelim/Activity III/ABATAYO_L_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class ABATAYO_L_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Leonard";
+        char letter = 'L';
+        float height = 1.899f;
+        int age = 18;
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(letter);
+        System.out.println(height);
+        System.out.println(age);
+        System.out.println(isOpen);
+    }
+}
