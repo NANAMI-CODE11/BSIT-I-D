a84de9a81dfc68ecebdc4e4ecf8ac7e25c41102a
VILLARINFRANZEN123 <margajafranzen@gmail.com>
2024-11-28 09:57:05 +0800

    Add files via upload

---
+++ Prelim/Activity III/VILLARIN_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class VILLARIN_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Julienne";
+        char letter = 'Y';
+        float height = 1.88f;
+        int age = 22;
+        boolean isOpen = true;
+
+        System.out.println(name);
+        System.out.println(letter);
+        System.out.println(height);
+        System.out.println(age);
+        System.out.println(isOpen);
+    }
+}
