e14897d31de6f67b56e1051ab8da3928888ad47b
precyromera <dragonsandprincess@gmail.com>
2024-11-28 09:43:56 +0800

    Add files via upload

---
+++ Midterm/Activity I/ROMERA_ACTIVITY_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ROMERA_ACTIVITY_I {
+    public static void main(String [] args){
+
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter the color");
+    
+    String color = sc.nextLine();
+        switch (color){
+            case "red":
+            System.out.println("This goes to Box #1");
+                break;
+            case "green":
+            System.out.println("This goes to Box #2");
+                break;
+            case "black":
+            System.out.println("This goes to Box #3");
+                break;
+            }
+        }
+    }
+
