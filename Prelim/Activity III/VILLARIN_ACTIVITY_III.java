7df6a69be5a235734e087bf6eb51afd4df71fbf4
VILLARINFRANZEN123 <margajafranzen@gmail.com>
2024-11-28 10:04:51 +0800

    Add files via upload

---
+++ Prelim/Activity III/VILLARIN_ACTIVITY_III.java
@@ -0,0 +1,14 @@
+class VILLARIN_ACTIVITY_III {
+    public static void main (String[] args) {
+        String Iwant = "PIATTOS";
+        int tagpila = 15;
+        double pilami = 6.2;
+        boolean weh = false;
+        char kinsa = 'F';
+        System.out.println(Iwant);
+        System.out.println(tagpila);
+        System.out.println(pilami);
+        System.out.println(weh);
+        System.out.println(kinsa);
+    }
+}
