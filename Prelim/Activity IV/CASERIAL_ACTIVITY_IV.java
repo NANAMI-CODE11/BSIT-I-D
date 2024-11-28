2a0616b5dd613c989f5bc69f89d94a4887a244a4
mariniel1 <caserialmariniel33@gmail.com>
2024-11-28 09:20:41 +0800

    Add files via upload

---
+++ Prelim/Activity IV/CASERIAL_ACTIVITY_IV.java
@@ -0,0 +1,12 @@
+public class CASERIAL_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String a = "My name is ";
+        String name = "Mariniel ";
+        String b = "and I am ";
+        int age = 18;
+        String c = " years old. ";
+        String d = a+name+b+age+c;
+
+        System.out.println(d);
+    }
+}
