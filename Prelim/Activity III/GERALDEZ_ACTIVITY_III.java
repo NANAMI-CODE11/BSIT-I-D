d4534f01b7c6fba83b615dc9172692a0d2825c50
thesassy01 <angelyngeraldez04@gmail.com>
2024-12-12 09:36:43 +0800

    Add files via upload

---
+++ Prelim/Activity III/GERALDEZ_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+class GERALDEZ_ACTIVITY_III {
+    public static void main(String [] args) {
+        String name = "Angelyn";
+        int age = 18;
+        float  height = 160.5f;
+        double weight = 80;
+        char letter = 'a';
+    
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        
+    
+    }
+}
