216c158c338f6603605ea732a1c1dcc622baa723
JOAMIE B. GENON <joamiegenon50@gmail.com>
2024-12-12 09:58:08 +0800

    Add files via upload

---
+++ Prelim/Activity III/GENON_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+class GENON_ACTIVITY_III {
+    public static void main(String[] args) {
+        String color = "blue";
+        int price = 200;
+        double height = 152.89;
+        char letter = 'c';
+        float weight = 45.9f
+
+        System.out.println(color);
+        System.out.println(price);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(weight);
+    }
+}
