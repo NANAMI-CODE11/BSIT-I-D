69ed6b64e687a1754f7f5ce52d33c62bd27c853d
Julienne Ybañez <166885721+nyanhachi@users.noreply.github.com>
2024-12-12 08:37:54 +0800

    Delete Prelim/Activity III/DELOS REYES_ACTIVITY_III.java

--- Prelim/Activity III/DELOS REYES_ACTIVITY_III.java
+++
@@ -1,9 +0,0 @@
- class DELOS_REYES_ACTIVITY_III {
-    public static void main (String [] args){
-       int x = 5;
-        while (x >= 0){
-            System.out.println(x);
-            x--;
-        }
-    }
-}
