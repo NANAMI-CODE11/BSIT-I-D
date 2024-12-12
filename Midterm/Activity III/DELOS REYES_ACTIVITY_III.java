9c48f621947808bf94e39f25a52397962b6e51ab
Axcel Delos Reyes <axceldelosreyes74@gmail.com>
2024-12-12 08:33:44 +0800

    Add files via upload

---
+++ Midterm/Activity III/DELOS REYES_ACTIVITY_III.java
@@ -0,0 +1,9 @@
+ class DELOS_REYES_ACTIVITY_III {
+    public static void main (String [] args){
+       int x = 5;
+        while (x >= 0){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
