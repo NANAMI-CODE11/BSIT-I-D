0ab88bcf9634e31ab04858d0657ec8eb82029926
Mathew Trocio Beltran <mathewbeltran27@gmail.com>
2024-12-14 19:50:37 +0800

    Add files via upload

---
+++ Prelim/Activity IV/BELTRAN_ACTIVITY_IV.java
@@ -0,0 +1,12 @@
+public class BELTRAN_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String a = "My name is ";
+        String name = "Mathew ";
+        String b = "and I am ";
+        int age = 20;
+        String c = " years old. ";
+        String d = a+name+b+age+c;
+
+        System.out.println(d);
+    }
+}
