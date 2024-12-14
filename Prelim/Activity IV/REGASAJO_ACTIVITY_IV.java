cbfff43235c6d5326e049eb0105e4add7e24618b
nicoleregasajo <regasajoglaizanicole94@gmail.com>
2024-12-14 20:02:41 +0800

    Add files via upload

---
+++ Prelim/Activity IV/REGASAJO_ACTIVITY_IV.java
@@ -0,0 +1,13 @@
+public class REGASAJO_ACTIVITY_IV {
+    public static void main(String [] args){
+
+        String name1 = "My name is";
+        String name2 =  "Glaiza Nicole";
+        String name3 =  "and I am";
+        int age = 18;
+        String name4 = "years old.";
+
+        System.out.println(name1+" "+name2+" "+name3+" "+age+" "+name4);
+    }
+    
+}
