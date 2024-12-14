e5e7c60cc497812ce1ab8de2b72d7104b89d488a
Mathew Trocio Beltran <mathewbeltran27@gmail.com>
2024-12-14 19:49:14 +0800

    Add files via upload

---
+++ Prelim/Activity III/BELTRAN_ACTIVITY_III.java
@@ -0,0 +1,15 @@
+public class    
+    public static void main (String [] args){
+        int var1 = 100;
+        String student = "Elboyins";
+        boolean isStudent = true;
+        char characterGrade = 'A';
+        float balance = 400.000f;
+
+        System.out.println(var1);
+        System.out.println(student);
+        System.out.println(isStudent);
+        System.out.println(characterGrade);
+        System.out.println(balance);
+    }
+}
