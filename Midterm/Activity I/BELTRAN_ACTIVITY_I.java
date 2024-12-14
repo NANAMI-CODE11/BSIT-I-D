451b14b29f4677c03dc568b55bd6bf930aab63ff
Mathew Trocio Beltran <mathewbeltran27@gmail.com>
2024-12-14 19:57:55 +0800

    Add files via upload
    love you sir!

---
+++ Midterm/Activity I/BELTRAN_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class BELTRAN_ACTIVITY_I {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a color among red, green, or black: ");
+    String color = sc.nextLine();
+        switch (color) {
+            case "red":
+            System.out.println("Box #1");    
+                break;
+            case "green":
+            System.out.println("Box #2");
+                break;
+            case "black":
+            System.out.println("Box #3");
+                break;
+        }
+    }
+}
