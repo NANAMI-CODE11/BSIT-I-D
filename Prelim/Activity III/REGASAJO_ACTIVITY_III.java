4b74a627f45d1c677173518da9abffbd2a648193
nicoleregasajo <regasajoglaizanicole94@gmail.com>
2024-12-14 20:24:21 +0800

    Add files via upload

---
+++ Prelim/Activity III/REGASAJO_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+class REGASAJO_ACTIVITY_III {
+    public static void main(String [] args) {
+        String name = "Glaiza Nicole";
+        int age = 18;
+        float  height = 154.5f;
+        double weight = 52;
+        char letter = 'n';
+    
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        
+    
+    }
+}
