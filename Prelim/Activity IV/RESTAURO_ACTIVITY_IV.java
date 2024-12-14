69a36691d68ef7406f30a3700e35b39eebb686ec
urninin05 <alfeeninarestauro@gmail.com>
2024-12-14 19:33:12 +0800

    Add files via upload

---
+++ Prelim/Activity IV/RESTAURO_ACTIVITY_IV.java
@@ -0,0 +1,12 @@
+public class RESTAURO_ACTIVITY_IV {
+    public static void main(String[] args) {
+        String a = "My name is ";
+        String name = "Alfee Nina ";
+        String b = "and I am ";
+        int age = 19;
+        String c = " years old. ";
+        String d = a+name+b+age+c;
+
+        System.out.println(d);
+    }
+}
