1b198ca3ec9e837f5921a817993e7ecb8db8e335
Richard Birao Jr <richardbiraooo@gmail.com>
2024-12-17 01:21:18 -0800

    Add files via upload

---
+++ Prelim/Activity III/BIRAO_ACTIVITY_III.java
@@ -0,0 +1,18 @@
+class BIRAO_ACTIVITY_III {
+    public static void main(String [] args) {
+        String name = "Richard Birao";
+        int age = 19;
+        float  height = 160.5f;
+        double weight = 80;
+        char letter = 'z';
+    
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(weight);
+        System.out.println(letter);
+        
+    
+    }
+}
