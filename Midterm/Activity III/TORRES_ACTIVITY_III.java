92aa55e6e65e58b5868046c9dc36d3de147f4695
carltorres143 <carljaytorres143@gmail.com>
2024-12-30 12:39:32 +0800

    Add files via upload

--- Midterm/Activity III/TORRES_ACTIVITY_III.java
+++ Midterm/Activity III/TORRES_ACTIVITY_III.java
@@ -1,1 +1,14 @@
-
+public class TORRES_ACTIVITY_III {
+    public static void main(String[] args) {
+        String name = "Carltorres";
+        int age = 22;
+        double height = 5.0;
+        boolean gege = (false);
+        char lang= 'A';
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(gege);
+        System.out.println(lang);
+    }
+}
