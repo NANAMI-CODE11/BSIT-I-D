ab145d3c74d8491e286e08fb3a418112e61cae30
JOAMIE B. GENON <joamiegenon50@gmail.com>
2025-01-16 09:43:10 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/GENON_ACTIVITY_I.java
@@ -0,0 +1,13 @@
+public class GENON_ACTIVITY_I {
+
+    public static void main(String[] args) {
+        String [] names = new String [5];
+        names [0] = "May";
+        names [1] = "April Jean";
+        names [2] = "Apple Mary";
+        names [3] = "Seren";
+        names [4] = "Primo";
+
+        System.out.println(names [3]);
+    }
+}
