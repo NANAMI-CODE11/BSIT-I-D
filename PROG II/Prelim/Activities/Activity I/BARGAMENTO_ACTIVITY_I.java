0a094a2423377f29184a1896bf066a75aee472d9
Cristy Mae Bargamento <cristymaebargamento@gmail.com>
2025-01-16 10:05:55 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/BARGAMENTO_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BARGAMENTO_ACTIVITY_I{
+  public static void main(String [] args ){
+    String [] names = new String [5];
+    names [0] = ("Jane");
+    names [1] = ("Eric");
+    names [2] = ("Yuri");
+    names [3] = ("Kefier");
+    names [4] = ("Jayjay");
+    System.out.println(names[3]);
+  }
+     
+
+}
