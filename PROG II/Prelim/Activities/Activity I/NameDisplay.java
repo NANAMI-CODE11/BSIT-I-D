582f4dc2e89dc54688c8b24a9162c3ec2c8358a5
John Bambi Lebosada <johnbambilebosada@gmail.com>
2025-01-16 09:51:53 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/NameDisplay.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NameDisplay {
+    public static void main(String[] args) {
+        
+        String[] names = new String[5];
+        Scanner scanner = new Scanner(System.in);
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+        
+        System.out.print("Enter a number (1-5) to display the name: ");
+        int index = scanner.nextInt() - 1;
+        
+        if (index >= 0 && index < 5) {
+            System.out.println("The selected name is: " + names[index]);
+        } else {
+            System.out.println("Invalid number entered.");
+        }
+        
+        scanner.close();
+    }
+}
