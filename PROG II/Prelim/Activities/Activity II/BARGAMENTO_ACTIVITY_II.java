b3fd19a49ae5a8c0ba861ca36335a42b1df5601f
Cristy Mae Bargamento <cristymaebargamento@gmail.com>
2025-01-23 09:18:09 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/BARGAMENTO_ACTIVITY_II.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BARGAMENTO_ACTIVITY_II {
+    public static void main(String[] args) {
+        
+        double[] scores = new double[10];
+        double sum = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 10 scores:");
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print("Score " + (i + 1) + ": ");
+            scores[i] = scanner.nextDouble();
+            sum += scores[i];
+        }
+
+        double average = sum / scores.length;
+
+        System.out.println("The average is: " + average);
+
+        scanner.close();
+    }
+}
