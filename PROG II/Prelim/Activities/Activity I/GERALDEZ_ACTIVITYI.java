604c99e2f772bd37cabd75cea28a3021f604f12a
thesassy01 <angelyngeraldez04@gmail.com>
2025-01-23 09:33:15 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/GERALDEZ_ACTIVITYI.java
@@ -0,0 +1,14 @@
+import.java.util.Scanner;
+
+public class GERALDEZ_ACTIVITY_I{
+
+    public static void main( String [] args){
+
+        Scanner sc = new Scanner System.In ;
+        String[] names= new String [5];
+        System.out.println("Enter name 5 times");
+        for (int i= o; i <=4r; i++) {
+            
+        }
+    }
+}
