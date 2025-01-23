bbe30567013727a6742bfb52fb569beb3c4104a8
Zierah Alfante <alfantezierah@gmail.com>
2025-01-23 09:18:54 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/ALFANTE_ACTIVITY_II.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ALFANTE_ACTIVITY_II {
+
+        public static void main(String[] args) {
+            
+            double[] scores = new double[10];;
+            Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please Enter 10 scores: ");
+
+            for(int i = 0; i < scores.length; i++){
+                System.out.print("Score" + (i + 1) + ":");
+                scores[i] = scanner.nextDouble ();
+
+            }
+
+            double sum = 0;
+                for(double score : scores){
+                    sum += score;
+                }
+
+                double average = sum / scores.length;
+
+            System.out.printf("The Average of the Entered scores is: %.2f%n", average);
+
+            scanner.close();
+        } 
+
+    }
+
