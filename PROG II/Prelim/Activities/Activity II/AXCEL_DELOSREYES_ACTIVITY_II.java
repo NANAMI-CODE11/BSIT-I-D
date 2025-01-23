7eb6e93771199edac9b43fac243f61eeb232efc4
Axcel Delos Reyes <axceldelosreyes74@gmail.com>
2025-01-23 09:28:44 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/AXCEL_DELOSREYES_ACTIVITY_II.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class AXCEL_DELOSREYES_ACTIVITY_II {
+    public static void main(String[] args) {
+        double[] scores = new double[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter 10 scores:");
+
+        for (int i = 0; i < 10; i++) {
+            while (true) {
+                System.out.print("Enter score " + (i + 1) + ": ");
+                if (scanner.hasNextDouble()) {
+                    scores[i] = scanner.nextDouble();
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a numeric value.");
+                    scanner.next();
+                }
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += scores[i];
+        }
+        double averageScore = sum / 10;
+
+        System.out.println("The average score is: " + averageScore);
+        
+        scanner.close();
+    }
+}
