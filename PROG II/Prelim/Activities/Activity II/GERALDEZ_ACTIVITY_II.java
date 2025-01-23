b18940a4a78b5cd5b9937a274ffaedc2bdd9a706
thesassy01 <angelyngeraldez04@gmail.com>
2025-01-23 09:29:13 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/GERALDEZ_ACTIVITY_II.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class GERALDEZ_ACTIVITY_II {
+    public static void main(String[] args) {
+        
+        double[] scores = new double[10];
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Please enter 10 scores:");
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print("Score " + (i + 1) + ": ");
+            scores[i] = scanner.nextDouble();
+        }
+
+        
+        double sum = 0;
+        for (double score : scores) {
+            sum += score;
+        }
+        double average = sum / scores.length;
+
+        
+        System.out.printf("The average of the entered scores is: %.2f%n", average);
+
+        
+        scanner.close();
+    }
+}
