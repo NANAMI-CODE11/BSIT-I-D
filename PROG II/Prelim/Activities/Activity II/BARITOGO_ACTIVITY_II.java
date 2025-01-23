31a128e86cbcf109f266b54280be65f8ee60fb38
carltorres143 <carljaytorres143@gmail.com>
2025-01-23 09:22:02 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/BARITOGO_ACTIVITY_II.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BARITOGO_ACTIVITY_II {
+    public static void main(String[] args) {
+        double[] scores = new double[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 10 scores:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Score " + (i + 1) + ": ");
+            scores[i] = scanner.nextDouble();
+        }
+
+        double total = 0;
+        for (double score : scores) {
+            total += score;
+        }
+
+        double average = total / scores.length;
+        System.out.println("\nThe average of the entered scores is: " + average);
+    }
+}
