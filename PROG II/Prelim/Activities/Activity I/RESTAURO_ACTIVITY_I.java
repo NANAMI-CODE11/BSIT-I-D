daea4220e79823f6318c2c2a173e0d9c95bcee86
urninin05 <alfeeninarestauro@gmail.com>
2025-01-23 09:30:58 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity I/RESTAURO_ACTIVITY_I.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class RESTAURO_ACTIVITY_I { 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[5];
+
+        System.out.println("Enter 5 names:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = scanner.nextLine();
+        }
+
+        while (true) {
+            System.out.print("Enter a number between 1 and 5 to display a name: ");
+            if (scanner.hasNextInt()) {
+                int index = scanner.nextInt();
+                if (index >= 1 && index <= 5) {
+                    System.out.println("The name at position " + index + " is: " + names[index - 1]);
+                    break;
+                } else {
+                    System.out.println("Please enter a number between 1 and 5.");
+                }
+            } else {
+                System.out.println("Invalid input! Please enter a numeric value.");
+                scanner.next();
+            }
+        }
+
+        scanner.close();
+    }
+}
+
+
