1b1b6ea435c40ee690e708bb8f36690a16d3fa0d
Leonard Abatayo <leonardabatayo5@gmail.com>
2025-01-23 09:19:13 +0800

    Update and rename ABATAYO_ACTIVITY_II.java to ABATAYO_L_ACTIVITY_II.java

--- PROG II/Prelim/Activities/Activity II/ABATAYO_L_ACTIVITY_II.java
+++ PROG II/Prelim/Activities/Activity II/ABATAYO_L_ACTIVITY_II.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ABATAYO_ACTIVITY_II {
+public class ABATAYO_L_ACTIVITY_II {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double[] scores = new double[10];
