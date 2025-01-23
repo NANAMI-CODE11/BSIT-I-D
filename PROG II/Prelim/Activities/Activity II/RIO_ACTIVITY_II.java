cdf06d9e6d57a008f8a81b13db0f7668e5a4d5d8
Renn933 <rennanboy345@gmail.com>
2025-01-23 09:29:01 +0800

    Add files via upload

---
+++ PROG II/Prelim/Activities/Activity II/RIO_ACTIVITY_II.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RIO_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] scores = new double[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter score " + (i + 1) + ": ");
+            scores[i] = scanner.nextDouble();
+        }
+
+        double sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += scores[i];
+        }
+        double average = sum / 10;
+        System.out.println("The average score is: " + average);
+
+        scanner.close();
+    }
+}    
