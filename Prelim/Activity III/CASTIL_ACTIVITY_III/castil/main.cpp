f9fa74ab16b5bce85bb0ad99d8cf03e28529b586
Junrey66 <junreycastil66@gmail.com>
2025-03-19 20:22:33 +0800

    Add files via upload

---
+++ Prelim/Activity III/CASTIL_ACTIVITY_III/castil/main.cpp
@@ -0,0 +1,15 @@
+class CASTIL_ACTIVITY_III {
+  public static void main(String[] args) {
+      String name = "Castil";
+      char letter = 'C';
+      float height = 1.899f;
+      int age = 18;
+      boolean isOpen = true;
+
+      System.out.println(name);
+      System.out.println(letter);
+      System.out.println(height);
+      System.out.println(age);
+      System.out.println(isOpen);
+  }
+}
