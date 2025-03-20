857ef116359f6ef5feff516ba3ef0e89b7abe0b9
pansoyjchub <jamesclydepansoy23@gmail.com>
2025-03-20 09:38:13 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/PANSOY_ACTIVITY_II.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PANSOY_ACTIVITY_II {
+
+    public static int feetToInches(int feet) {
+        return feet * 12;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+
+        try {
+            int feet = scanner.nextInt();
+            int inches = feetToInches(feet);
+            System.out.println("Inches: " + inches);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter an integer.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
