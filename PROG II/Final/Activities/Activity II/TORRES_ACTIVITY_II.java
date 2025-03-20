e33f62bc4b4dd223477370e8bba76c287ccb947e
carltorres143 <carljaytorres143@gmail.com>
2025-03-20 09:39:05 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/TORRES_ACTIVITY_II.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TORRES_ACTIVITY_II {  
+
+   
+    public static void convertToInches(int feet) {
+        int inches = feet * 12; 
+        System.out.println("Inches: " + inches); 
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      
+        System.out.print("Enter feet: ");
+        int feet = scanner.nextInt();
+
+    
+        convertToInches(feet);
+
+
+    }
+}
