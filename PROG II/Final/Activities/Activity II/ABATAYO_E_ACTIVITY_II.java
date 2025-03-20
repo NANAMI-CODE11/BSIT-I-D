1cce2fada0ff8cda46fca70705d9248e18547bb1
Elboyhub <elboyabatayo04@gmail.com>
2025-03-20 09:42:54 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/ABATAYO_E_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ABATAYO_E_ACTIVITY_II {
+
+    public static int convertFeetToInches(int feet) {
+        return feet * 12;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+        int feet = scanner.nextInt();
+        int inches = convertFeetToInches(feet);
+        System.out.println("Inches: " + inches);
+        scanner.close();
+    }
+}
