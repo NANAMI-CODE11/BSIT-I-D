7ae67eca7305d05d41cd287929023e5bc2a86953
mariniel1 <caserialmariniel33@gmail.com>
2025-03-20 09:39:28 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/CASERIAL_ACTIVITY_II.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CASERIAL_ACTIVITY_II {
+
+    public static double convertToInches(double feet) {
+      return feet * 12;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of feet: ");
+        double feet = scanner.nextDouble();
+
+        System.out.println("Inches: " + convertToInches(feet));
+
+        scanner.close();
+    }
+}
