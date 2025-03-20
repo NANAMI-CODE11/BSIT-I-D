95656b95e2256c6e9d61c6485541999385075d28
fretchecabatas08 <cabatasfretche08@gmail.com>
2025-03-20 09:38:20 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/CABATAS_ACTIVTY_II.java
@@ -0,0 +1,13 @@
+public class CABATAS_ACTIVTY_II{
+static void inch( double feet){
+    double inch = feet * 12;
+    System.out.println(feet + " feet is equal to " + inch + " inches.");
+
+    
+}
+public static void main(String[] args) {
+    inch(12);
+    
+
+}
+}
