2320ba4349fd9eeecaacee16a06644b5be71ba3c
Jeff Vincent Gimena <jepoygimena325@gmail.com>
2025-03-20 09:40:17 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/GIMENA_ACTIVITY_II.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GIMENA_ACTIVITY_II {  
+
+    public static void convertToInches(int feet) {
+        int inches = feet * 12; 
+        System.out.println("Inches: " + inches); 
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter feet: ");
+        int feet = scanner.nextInt();
+
+        convertToInches(feet);
+
+
+    }
+}
