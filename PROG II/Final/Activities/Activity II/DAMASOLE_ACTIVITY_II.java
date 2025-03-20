d29a014f54b0e08df307f9e64731cd32418d5fe1
pewkiepie27 <yeeldamasole@gmail.com>
2025-03-20 09:38:01 +0800

    Add files via upload

---
+++ PROG II/Final/Activities/Activity II/DAMASOLE_ACTIVITY_II.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class DAMASOLE_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number that you want to convert into inches");
+
+        double feet = sc.nextDouble();
+
+        double inches = feet * 12;
+
+        System.out.println(feet + "feet is equal to " + inches);
+        
+    }
+}
